1835126c7c09fd6cb25fa721d608fabd576ab2b4
Shrihan Chaurasia <78851287+chaurasiashrihan0206@users.noreply.github.com>
2021-12-13 17:54:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+objects= [];
+
 img="";
 
 status= "";
@@ -24,19 +26,32 @@ function gotResult(error , results){
         console.log(error);
     }
     console.log(results);
+    objects= results;
 }
 
 function draw(){
     image(img , 0 , 0 , 640 , 420);
+    if(status!=""){
+    for(i=0 ; i<objects.length ; i++){
+    document.getElementById("status").innerHTML= "Status:Object Detected"
     fill("#FF0000");
-    text("Dog" , 200 , 50);
+    percent= floor(objects[i].confidence*100);
+    text(objects[i].label + " " + percent + "%" , objects[i].x + 100 , objects[i].y , 100 , 50);
     noFill();
     stroke("#FF0000");
-    rect(100 , 50 , 300 , 350); 
+    rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height); 
+    }
 
-    fill("#0000FF");
-    text("Cat" , 500 , 50);
-    noFill();
-    stroke("#0000FF");
-    rect(300 , 55 , 300 , 350);
+    }
+    //fill("#FF0000");
+    //text("Dog" , 200 , 50);
+    //noFill();
+    //stroke("#FF0000");
+    //rect(100 , 50 , 300 , 350); 
+
+    //fill("#0000FF");
+    //text("Cat" , 500 , 50);
+    //noFill();
+    //stroke("#0000FF");
+    //rect(300 , 55 , 300 , 350);
 }
